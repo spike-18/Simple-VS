5821e762ce7b63f04a7dc74144a4ed23dce647fc
Kirill Madorskii <madorskii.kv@phystech.edu>
2025-06-17 22:45:39 +0300

    gradients calculated

--- src/simple_vs/util.py
+++ src/simple_vs/util.py
@@ -1,1 +1,13 @@
 import numpy as np
+
+
+def get_gradient_map(image: np.ndarray) -> np.ndarray:
+    x, y = image.shape[0], image.shape[1]
+    output = np.zeros((x, y, 2), dtype=np.float32)
+
+    output[1:, :, 0] = image[1:, :] - image[: x - 1, :]
+    output[:, 1:, 1] = image[:, 1:] - image[:, : y - 1]
+    output[0, :, 0] = image[1, :] - image[0, :]
+    output[:, 0, 1] = image[:, 1] - image[:, 0]
+
+    return output
