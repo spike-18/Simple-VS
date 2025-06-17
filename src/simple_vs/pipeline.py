095f19526b5d323c3144d45736c1ece41f19519e
Kirill Madorskii <madorskii.kv@phystech.edu>
2025-06-18 01:33:55 +0300

    edges detection

--- src/simple_vs/pipeline.py
+++ src/simple_vs/pipeline.py
@@ -2,23 +2,51 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage.color import rgb2gray, rgba2rgb
 
-from .util import get_gradient_map
+from .util import get_gradient_map, sobel_grad
 
 
 def process_image(input_img: np.ndarray) -> np.ndarray:
-    gray_img = rgb2gray(rgba2rgb(input_img))
+    edges_map = get_edges(input_img, threshold=0.1)
 
-    grad_map = get_gradient_map(gray_img)
-    grad_norm_map = np.linalg.norm(grad_map, axis=2)
+
+
+def get_edges(image: np.ndarray, threshold = 0.15) -> np.ndarray:
+    gray_img = rgb2gray(rgba2rgb(image)) if image.shape[2] == 4 else rgb2gray(image)
+
+    grad_map = sobel_grad(gray_img)
+    grad_norm_map = np.linalg.norm(grad_map, ord=2, axis=2)
+    grad_norm_map = (grad_norm_map - np.min(grad_norm_map)) / (np.max(grad_norm_map) -
+                                                               np.min(grad_norm_map))
+
+    grad_angle_map = np.arctan2(grad_map[:, :, 1], grad_map[:, :, 0])
+
+    edges_map = (grad_norm_map > threshold)
+
+    angle_thresh = np.deg2rad(30)
+
+    vertical_edges = edges_map & (
+        (np.abs(grad_angle_map - np.pi/2) < angle_thresh) |
+        (np.abs(grad_angle_map + np.pi/2) < angle_thresh)
+    )
+
+    horizontal_edges = ~vertical_edges & edges_map
+
+    color_edges = np.zeros((*vertical_edges.shape, 3), dtype=np.float32)
+    color_edges[vertical_edges] = [0, 1, 0]   # Green
+    color_edges[horizontal_edges] = [1, 0, 0] # Red
+
 
     fig, axes = plt.subplots(1, 2, figsize=(18, 9))
-    axes[0].imshow(input_img)
+
+    axes[0].imshow(image)
     axes[0].set_title("Input Image")
-    axes[1].imshow(grad_norm_map)
-    axes[1].set_title("Gradient Norm Map")
+    axes[1].imshow(color_edges)
+    axes[1].set_title("Edges")
+
 
     for ax in axes:
         ax.axis("off")
 
     plt.tight_layout()
+    plt.savefig("example_images/edges_output.png")
     plt.show()
--- src/simple_vs/util.py
+++ src/simple_vs/util.py
@@ -11,3 +11,26 @@ def get_gradient_map(image: np.ndarray) -> np.ndarray:
     output[:, 0, 1] = image[:, 1] - image[:, 0]
 
     return output
+
+
+def sobel_grad(image: np.ndarray) -> np.ndarray:
+    x, y = image.shape[0], image.shape[1]
+    output = np.zeros((x, y, 2), dtype=np.float32)
+
+    output[1:x-1, 1:y-1, 0] = (-image[:x-2, 2:y] + image[2:x,2:y] - 2*image[:x-2, 1:y-1] \
+                                    + 2*image[2:x, 1:y-1] - image[:x-2, 0:y-2] + image[2:x,0:y-2])
+
+    output[1:x-1, 1:y-1, 1] = (-image[:x-2, 2:y] - image[2:x,2:y] - 2*image[1:x-1, 2:y] \
+                                    + 2*image[1:x-1, 0:y-2] + image[:x-2, 0:y-2] + image[2:x,0:y-2])
+
+    output[0, :, 0] = image[1, :] - image[0, :]
+    output[x-1, :, 0] = image[x-1, :] - image[x-2, :]
+    output[:x-1, 0, 0] = image[1:, 0] - image[:x-1, 0]
+    output[:x-1, y-1, 0] = image[1:, y-1] - image[:x-1, y-1]
+
+    output[:, 0, 1] = image[:, 1] - image[:, 0]
+    output[:, y-1, 1] = image[:, y-1] - image[:, y-2]
+    output[0, :y-1, 1] = image[0, 1:] - image[0, :y-1]
+    output[x-1, :y-1, 1] = image[x-1, 1:] - image[x-1, :y-1]
+
+    return output
