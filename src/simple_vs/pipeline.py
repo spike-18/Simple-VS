57cf8a4fe7d33d531332c68c133857fee66589b4
Kirill Madorskii <madorskii.kv@phystech.edu>
2025-06-21 00:38:32 +0300

    plotting stage 1

--- src/simple_vs/pipeline.py
+++ src/simple_vs/pipeline.py
@@ -1,64 +1,69 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from skimage.color import rgb2gray, rgba2rgb
 
-from .util import get_gradient_map, sobel_grad
+from .util import plot_map, sobel_grad, plot_interpretation
 
 
 def process_image(input_img: np.ndarray) -> np.ndarray:
-    vert_edges, horiz_edges = get_edges(input_img, threshold=0.1)
-    sat_map = get_figures(input_img)
+    image = rgba2rgb(input_img) if input_img.shape[2] == 4 else input_img
+
+    image = image[::50, ::50]
+
+    height, width = image.shape[0], image.shape[1]
+
+    x_3d = np.zeros((1, height*width))
+    y_3d = np.zeros((1, height*width))
+    z_3d = np.zeros((1, height*width))
+
+    z_3d = np.arange(0, height, 1)
+    x_3d = np.arange(0, width, 1)
+
+    x_3d, z_3d = np.meshgrid(x_3d, z_3d)
+
+    x_3d = x_3d.reshape((1, height*width))
+    z_3d = z_3d.reshape((1, height*width))
+
+    vert_edges, horiz_edges = get_edges(image, threshold=0.1)
+    fig_map = get_figures(image)
+
+    plot_interpretation(image, (x_3d, y_3d, z_3d))
 
 
-def get_edges(image: np.ndarray, threshold = 0.15) -> tuple[np.ndarray, np.ndarray]:
+def get_edges(image: np.ndarray, threshold=0.15) -> tuple[np.ndarray, np.ndarray]:
     gray_img = rgb2gray(rgba2rgb(image)) if image.shape[2] == 4 else rgb2gray(image)
 
     grad_map = sobel_grad(gray_img)
     grad_norm_map = np.linalg.norm(grad_map, ord=2, axis=2)
-    grad_norm_map = (grad_norm_map - np.min(grad_norm_map)) / (np.max(grad_norm_map) -
-                                                               np.min(grad_norm_map))
+    grad_norm_map = (grad_norm_map - np.min(grad_norm_map)) / (
+        np.max(grad_norm_map) - np.min(grad_norm_map)
+    )
 
     grad_angle_map = np.arctan2(grad_map[:, :, 1], grad_map[:, :, 0])
 
-    edges_map = (grad_norm_map > threshold)
+    edges_map = grad_norm_map > threshold
 
     angle_thresh = np.deg2rad(30)
 
     vertical_edges = edges_map & (
-        (np.abs(grad_angle_map - np.pi/2) < angle_thresh) |
-        (np.abs(grad_angle_map + np.pi/2) < angle_thresh)
+        (np.abs(grad_angle_map - np.pi / 2) < angle_thresh)
+        | (np.abs(grad_angle_map + np.pi / 2) < angle_thresh)
     )
 
     horizontal_edges = ~vertical_edges & edges_map
 
     # color_edges = np.zeros((*vertical_edges.shape, 3), dtype=np.float32)
-    # color_edges[vertical_edges] = [0, 1, 0]   # Green
-    # color_edges[horizontal_edges] = [1, 0, 0] # Red
+    # color_edges[vertical_edges] = [0, 1, 0]  # Green
+    # color_edges[horizontal_edges] = [1, 0, 0]  # Red
 
-    # fig, axes = plt.subplots(1, 2, figsize=(18, 9))
-
-    # axes[0].imshow(image)
-    # axes[0].set_title("Input Image")
-    # axes[1].imshow(color_edges)
-    # axes[1].set_title("Edges")
-
-    # for ax in axes:
-    #     ax.axis("off")
-
-    # plt.tight_layout()
-    # plt.savefig("example_images/edges_output.png")
-    # plt.show()
+    # plot_map(image, color_edges, map_name="Edges", save=False)
 
     return vertical_edges, horizontal_edges
 
 
-def get_figures(image: np.ndarray) -> np.ndarray:
-
+def get_figures(image: np.ndarray, threshold=0.3) -> np.ndarray:
     image = rgba2rgb(image) if image.shape[2] == 4 else image
 
-    # Normalize the image to [0, 1]
     norm_image = image.astype(np.float32) / 255.0
-
 
     max_c = np.max(norm_image, axis=2)
     min_c = np.min(norm_image, axis=2)
@@ -67,17 +72,8 @@ def get_figures(image: np.ndarray) -> np.ndarray:
     sat_map = delta / max_c
     sat_map[np.isinf(sat_map)] = 0
 
-    fig, axes = plt.subplots(1, 2, figsize=(18, 9))
+    fig_map = sat_map > threshold
 
-    axes[0].imshow(image)
-    axes[0].set_title("Input Image")
-    axes[1].imshow(sat_map)
-    axes[1].set_title("Saturation map")
+    # plot_map(image, fig_map, map_name="Figures", save=False)
 
-    for ax in axes:
-        ax.axis("off")
-
-    plt.tight_layout()
-    plt.show()
-
-    return sat_map
+    return fig_map
--- src/simple_vs/util.py
+++ src/simple_vs/util.py
@@ -1,4 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import cm
+from matplotlib.colors import LightSource
 
 
 def get_gradient_map(image: np.ndarray) -> np.ndarray:
@@ -17,20 +20,80 @@ def sobel_grad(image: np.ndarray) -> np.ndarray:
     x, y = image.shape[0], image.shape[1]
     output = np.zeros((x, y, 2), dtype=np.float32)
 
-    output[1:x-1, 1:y-1, 0] = (-image[:x-2, 2:y] + image[2:x,2:y] - 2*image[:x-2, 1:y-1] \
-                                    + 2*image[2:x, 1:y-1] - image[:x-2, 0:y-2] + image[2:x,0:y-2])
+    output[1 : x - 1, 1 : y - 1, 0] = (
+        -image[: x - 2, 2:y]
+        + image[2:x, 2:y]
+        - 2 * image[: x - 2, 1 : y - 1]
+        + 2 * image[2:x, 1 : y - 1]
+        - image[: x - 2, 0 : y - 2]
+        + image[2:x, 0 : y - 2]
+    )
 
-    output[1:x-1, 1:y-1, 1] = (-image[:x-2, 2:y] - image[2:x,2:y] - 2*image[1:x-1, 2:y] \
-                                    + 2*image[1:x-1, 0:y-2] + image[:x-2, 0:y-2] + image[2:x,0:y-2])
+    output[1 : x - 1, 1 : y - 1, 1] = (
+        -image[: x - 2, 2:y]
+        - image[2:x, 2:y]
+        - 2 * image[1 : x - 1, 2:y]
+        + 2 * image[1 : x - 1, 0 : y - 2]
+        + image[: x - 2, 0 : y - 2]
+        + image[2:x, 0 : y - 2]
+    )
 
     output[0, :, 0] = image[1, :] - image[0, :]
-    output[x-1, :, 0] = image[x-1, :] - image[x-2, :]
-    output[:x-1, 0, 0] = image[1:, 0] - image[:x-1, 0]
-    output[:x-1, y-1, 0] = image[1:, y-1] - image[:x-1, y-1]
+    output[x - 1, :, 0] = image[x - 1, :] - image[x - 2, :]
+    output[: x - 1, 0, 0] = image[1:, 0] - image[: x - 1, 0]
+    output[: x - 1, y - 1, 0] = image[1:, y - 1] - image[: x - 1, y - 1]
 
     output[:, 0, 1] = image[:, 1] - image[:, 0]
-    output[:, y-1, 1] = image[:, y-1] - image[:, y-2]
-    output[0, :y-1, 1] = image[0, 1:] - image[0, :y-1]
-    output[x-1, :y-1, 1] = image[x-1, 1:] - image[x-1, :y-1]
+    output[:, y - 1, 1] = image[:, y - 1] - image[:, y - 2]
+    output[0, : y - 1, 1] = image[0, 1:] - image[0, : y - 1]
+    output[x - 1, : y - 1, 1] = image[x - 1, 1:] - image[x - 1, : y - 1]
 
     return output
+
+
+def plot_map(image: np.ndarray, image_map: np.ndarray, save: bool, map_name="Map") -> None:
+    fig, axes = plt.subplots(1, 2, figsize=(18, 9))
+
+    axes[0].imshow(image)
+    axes[0].set_title("Input Image")
+    axes[1].imshow(image_map, cmap="gray")
+    axes[1].set_title(map_name)
+
+    for ax in axes:
+        ax.axis("off")
+
+    plt.tight_layout()
+    if save:
+        plt.savefig("example_images/" + map_name)
+    plt.show()
+
+
+def plot_interpretation(image: np.ndarray, coords: tuple[np.array]) -> None:
+
+    x_img = coords[0]
+    y_img = coords[1]
+    z_img = coords[2]
+
+    H, W = image.shape[0], image.shape[1]
+
+    X_3d = np.linspace(np.min(x_img), np.max(x_img), W)
+    Y_3d = np.linspace(np.min(z_img), np.max(z_img), H)
+
+    X, Y = np.meshgrid(X_3d, Y_3d)
+    Z = np.full((Y_3d.size, X_3d.size), np.nan)
+
+    
+
+
+
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection="3d")
+
+    ax.plot_surface(X, Y, Z, rstride=1)
+    ax.scatter(x_img, z_img, y_img, color="red")
+
+    ax.set_xlabel("X")
+    ax.set_ylabel("Z")
+    ax.set_zlabel("Y")
+
+    plt.show()
